c7b7076fe5ed7107f2d0b66a39eefb56025de147
Yuyang Zhang <yzhang4@stuy.edu>
2017-04-03 08:24:48 -0400

    Updated Comments

--- Deque.java
+++ Deque.java
@@ -1,13 +1,17 @@
 public interface Deque<T>{
-    public T peekFront();
-    public T peekEnd();
+    public T peekFront(); //returns the first value in the array
+    public T peekEnd(); //returns the last value in the array
     
-    public void addFront( T q);
-    public void addEnd(T q);
+    public void addFront( T q); //adds a value to the front of the array
+    public void addEnd(T q); //adds a value to the end of the array
     
-    public T removeFront();
-    public T removeEnd();
+    public T removeFront(); //removes a value from the front of the array
+    public T removeEnd(); //remove a value from the end of the array
 
-    public int size();
-    public boolean contains(T x);
+    public void removeFirst(T q); //removes the first occurance of the value in the array that is equal to q.
+    public void removeLast(T q); //removes the last occurance of the value in the array that is equal to q.
+
+    public int size(); //the number of values in the array
+    public boolean contains(T x); //if the value is present in the array. 
+    public int atIndex(T q); //returns the index of the first occurance of q in the array.
 }

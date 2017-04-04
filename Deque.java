aab81cafccf1fa1aee22a4e638a0d378df592e4a
Yuyang Zhang <yzhang4@stuy.edu>
2017-04-03 20:46:47 -0400

    Updated Interface

--- Deque.java
+++ Deque.java
@@ -11,6 +11,9 @@ public interface Deque<T>{
     public void removeFirst(T q); //removes the first occurance of the value in the array that is equal to q.
     public void removeLast(T q); //removes the last occurance of the value in the array that is equal to q.
 
+    public T pollFront(); //error handling for removeFront
+    public T pollEnd(); //error handling for removeEnd
+
     public int size(); //the number of values in the array
     public boolean contains(T x); //if the value is present in the array. 
     public int atIndex(T q); //returns the index of the first occurance of q in the array.
